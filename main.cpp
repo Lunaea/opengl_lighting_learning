c5f8a18b03efe2160f082dc91e9780af0d2ae354
Lunaea <132797043+Lunaea@users.noreply.github.com>
2024-11-11 13:25:48 -0800

    Pass lightPos to shader uniform

--- main.cpp
+++ main.cpp
@@ -143,6 +143,7 @@ int main(int, char**){
         lightingShader.use();
         lightingShader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
         lightingShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
+        lightingShader.setVec3("lightPos", lightPos);
 
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
         glm::mat4 view = camera.GetViewMatrix();
