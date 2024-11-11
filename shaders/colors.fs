4f86899f4d59f7cb745d3ecc07fb65a7e40959fa
Lunaea <132797043+Lunaea@users.noreply.github.com>
2024-11-11 13:26:15 -0800

    Calculate diffuse lighting

--- shaders/colors.fs
+++ shaders/colors.fs
@@ -1,16 +1,24 @@
 #version 330 core
 out vec4 FragColor;
 
+in vec3 FragPos;
 in vec3 Normal;
 
+uniform vec3 lightPos;
 uniform vec3 objectColor;
 uniform vec3 lightColor;
 
 void main()
 {
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - FragPos);
+
+    float diff = max(dot(norm, lightDir), 0.0f);
+    vec3 diffuse = diff * lightColor;
+
     float ambientStrength = 0.1f;
     vec3 ambient = ambientStrength * lightColor;
 
-    vec3 result = ambient * objectColor;
+    vec3 result = (ambient + diffuse) * objectColor;
     FragColor = vec4(result, 1.0f);
 }
