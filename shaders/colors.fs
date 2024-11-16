83f801b9980767785deda6d9fd3af6c5e6c137a0
Lunaea <132797043+Lunaea@users.noreply.github.com>
2024-11-16 05:41:51 -0800

    Adjust shine factor

--- shaders/colors.fs
+++ shaders/colors.fs
@@ -15,16 +15,16 @@ void main()
     vec3 lightDir = normalize(lightPos - FragPos);
 
     float diff = max(dot(norm, lightDir), 0.0f);
-    vec3 diffuse = diff * lightColor * 1.8f;
+    vec3 diffuse = diff * lightColor;
 
     float ambientStrength = 0.3f;
     vec3 ambient = ambientStrength * lightColor;
 
-    float specularStrength = 3.0f;
+    float specularStrength = 0.5f;
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);
 
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0f), 32);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0f), 128);
     vec3 specular = specularStrength * spec * lightColor;
 
     vec3 result = (ambient + diffuse + specular) * objectColor;
