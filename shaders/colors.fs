8e333222b214428db1c40b85655e533a909e2923
Lunaea <132797043+Lunaea@users.noreply.github.com>
2024-11-16 06:12:43 -0800

    Implement shading in view space

--- shaders/colors.fs
+++ shaders/colors.fs
@@ -3,8 +3,9 @@ out vec4 FragColor;
 
 in vec3 FragPos;
 in vec3 Normal;
+in vec3 LightPos;
 
-uniform vec3 lightPos;
+// uniform vec3 lightPos;
 uniform vec3 objectColor;
 uniform vec3 lightColor;
 uniform vec3 viewPos;
@@ -12,19 +13,19 @@ uniform vec3 viewPos;
 void main()
 {
     vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(lightPos - FragPos);
+    vec3 lightDir = normalize(LightPos - FragPos);
 
     float diff = max(dot(norm, lightDir), 0.0f);
     vec3 diffuse = diff * lightColor;
 
-    float ambientStrength = 0.3f;
+    float ambientStrength = 0.1f;
     vec3 ambient = ambientStrength * lightColor;
 
     float specularStrength = 0.5f;
-    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 viewDir = normalize(-FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);
 
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0f), 128);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0f), 32);
     vec3 specular = specularStrength * spec * lightColor;
 
     vec3 result = (ambient + diffuse + specular) * objectColor;
