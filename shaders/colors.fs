33e47fe417eff3470964ba5df823adaa939fb880
Lunaea <132797043+Lunaea@users.noreply.github.com>
2024-11-28 07:27:04 -0800

    Utilize lighting functions in fragment shader

--- shaders/colors.fs
+++ shaders/colors.fs
@@ -61,39 +61,17 @@ vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir);
 
 void main()
 {   
-    vec3 lightDir = normalize(light.position - FragPos);
-
-    // spotlight
-    float theta = dot(lightDir, normalize(-light.direction));
-    float epsilon = light.cutOff - light.outerCutOff;
-    float intensity = clamp((theta - light.outerCutOff) / epsilon, 0.0f, 1.0f);
-
-    // diffuse
+    // properties
     vec3 norm = normalize(Normal);
-    float diff = max(dot(norm, lightDir), 0.0f);
-    vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords));
-
-    // ambient
-    vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
-
-    // specular
     vec3 viewDir = normalize(viewPos - FragPos);
-    vec3 reflectDir = reflect(-lightDir, norm);
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0f), material.shininess);
-    vec3 specular = light.specular * spec * vec3(texture(material.specular, TexCoords));
 
-    // emission
-    vec3 emission = vec3(texture(material.emission, TexCoords));
+    // Directional Lighting
+    vec3 result = CalcDirLight(dirLight, norm, viewDir);
 
-    // attenuation
-    float distance = length(light.position - FragPos);
-    float attenuation = 1.0f / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
+    // Point Lights
+    for (int i = 0; i < NR_POINT_LIGHTS; ++i)
+        result += CalcPointLight(pointLights[i], norm, FragPos, viewDir);
 
-    ambient *= attenuation;
-    diffuse *= (attenuation * intensity);
-    specular *= (attenuation * intensity);
-
-    vec3 result = ambient + diffuse + specular; // + emission;
     FragColor = vec4(result, 1.0f);
 }
 
