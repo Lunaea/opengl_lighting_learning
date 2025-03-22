44edafca97b92fb6be214f7def9719928b0dfa66
Lunaea <132797043+Lunaea@users.noreply.github.com>
2025-03-21 22:22:29 -0700

    Rewrite loadTexture

--- main.cpp
+++ main.cpp
@@ -352,39 +352,36 @@ unsigned int loadTexture(const char* path)
 {
     unsigned int textureID{};
     glGenTextures(1, &textureID);
-    glBindTexture(GL_TEXTURE_2D, textureID);
-    // set the texture wrapping/filtering options (on the currently bound texture object)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    // load and generate the texture
+
     int width{};
     int height{};
-    int nrChannels{};
+    int nrComponents{};
+    unsigned char* data = stbi_load(path, &width, &height, &nrComponents, 0);
 
-    stbi_set_flip_vertically_on_load(true);
-    unsigned char* data = stbi_load(path, &width, &height, &nrChannels, 0);
     if (data)
     {
-        GLenum format{};
-        if (nrChannels == 1)
+        GLenum format;
+        if (nrComponents == 1)
             format = GL_RED;
-        else if (nrChannels == 3)
+        else if (nrComponents == 3)
             format = GL_RGB;
-        else
+        else if (nrComponents == 4)
             format = GL_RGBA;
 
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, format, GL_UNSIGNED_BYTE, data);
-
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
 
-        stbi_image_free(data);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
+        stbi_image_free(data);
     }
     else
     {
-        std::cerr << "Failed to load texture\n";
+        std::cerr << "Texture failed to load at path: " << path << '\n';
         stbi_image_free(data);
     }
 
