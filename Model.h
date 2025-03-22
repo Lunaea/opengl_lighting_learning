78139a0a7124fb522246cc6a349e2ba3d4f49688
Lunaea <132797043+Lunaea@users.noreply.github.com>
2025-03-22 01:26:01 -0700

    Begin Implementing Model class

---
+++ Model.h
@@ -0,0 +1,40 @@
+#ifndef MODEL_H
+#define MODEL_H
+
+#include <glad/glad.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <map>
+
+#include "Mesh.h"
+#include "shader.h"
+#include "stb_image.h"
+
+class Model
+{
+public:
+    Model(char* path) { loadModel(path); }
+    void Draw(Shader& shader);
+
+private:
+    // model data
+    std::vector<Mesh> meshes{};
+    std::string directory{};
+
+    void loadMOdel(std::string path);
+    void processNode(aiNode* node, const aiScene* scene);
+    Mesh processMesh(aiMesh* mesh, const aiScene* scene);
+    std::vector<Texture> loadMaterialTextures(aiMaterial* mat, aiTextureType type, std::string typeName);
+};
+
+#endif
