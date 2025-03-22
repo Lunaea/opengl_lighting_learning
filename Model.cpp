350f2091edf1f0a0f5f30979cf4d30c47554be26
Lunaea <132797043+Lunaea@users.noreply.github.com>
2025-03-22 02:05:31 -0700

    Implement Model::processMesh vertices

--- Model.cpp
+++ Model.cpp
@@ -20,3 +20,52 @@ void Model::loadModel(std::string path)
 
     processNode(scene->mRootNode, scene);
 }
+
+void Model::processNode(aiNode* node, const aiScene* scene)
+{
+    // process all the node's meshes (if any)
+    for(u_int i = 0; i < node->mNumMeshes; ++i)
+    {
+        aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
+        meshes.push_back(processMesh(mesh, scene));
+    }
+    // then do the same for each of its children
+    for(u_int i = 0; i < node->mNumChildren; ++i)
+        processNode(node->mChildren[i], scene);
+}
+
+Mesh processMesh(aiMesh* mesh, const aiScene* scene)
+{
+    std::vector<Vertex> vertices{};
+    std::vector<unsigned int> indices{};
+    std::vector<Texture> textures{};
+
+    for(u_int i = 0; i < mesh->mNumVertices; ++i)
+    {
+        Vertex vertex{};
+        // process vertex positions, normals and texture coordinates
+        glm::vec3 vector{ mesh->mVertices[i].x, mesh->mVertices[i].y, mesh->mVertices[i].z };
+        vertex.Position = vector;
+        vertices.push_back(vertex);
+
+        vector = { mesh->mNormals[i].x, mesh->mNormals[i].y, mesh->mNormals[i].z };
+        vertex.Normal = vector;
+
+        if(mesh->mTextureCoords[0]) // does the mesh contain texture coordinates?
+        {
+            glm::vec2 vec{ mesh->mTextureCoords[0][i].x, mesh->mTextureCoords[0][i].y };
+            vertex.TexCoords = vec;
+        }
+        else
+            vertex.TexCoords = glm::vec2(0.0f);
+    }
+    // process indices
+    [...]
+    // process material
+    if(mesh->mMaterialIndex >= 0)
+    {
+        [...]
+    }
+
+    return Mesh(vertices, indices, textures);
+}
